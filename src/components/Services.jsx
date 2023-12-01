657932aecaa6d5709b4af6b6495bbeb8a6c4812d
ali saad ghanem <152627467+ahmednagy07@users.noreply.github.com>
2023-12-02 01:16:29 +0200

    initial commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -96,7 +96,10 @@ const Section = styled.section`
   @media screen and (max-width: 1080px) {
     .services {
       grid-template-columns: repeat(2, 1fr); /* Set to 2 columns for medium screens */
-      max-width: 332px; 
+      service {
+        max-width: 332px;
+      }
+  
       img {
         height: 300px;
         max-width: 300px;
